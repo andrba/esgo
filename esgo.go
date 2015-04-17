40941a60c1ceee328152fe6d2494a6e5fc644e52
Andrey <andrey.bazhutkin@gmail.com>
2015-04-17 16:01:32 +1000

    Refactoring

--- esgo.go
+++ esgo.go
@@ -5,7 +5,6 @@ import (
   "strings"
   "encoding/json"
   "net/http"
-  "io"
   "io/ioutil"
 )
 
@@ -16,78 +15,49 @@ func Configure(host string, port int) {
 }
 
 func Request(method, url, data string) (body []byte, err error) {
-  var response map[string]interface{}
-  var httpStatusCode int
-  var req *http.Request
+  var request *http.Request
+  var response *http.Response
 
-  req, err = http.NewRequest(method, server + url, nil)
+  if request, err = buildRequest(method, server + url, data); err != nil {
+    return
+  }
+
+  response, err = http.DefaultClient.Do(request)
+  defer response.Body.Close()
+
+  if err != nil {
+    return
+  }
+
+  if body, err = ioutil.ReadAll(response.Body); err != nil {
+    return
+  }
+
+  if response.StatusCode > 304 {
+
+    var v map[string]interface{}
+
+    jsonBody := json.Unmarshal(body, &v)
+
+    if jsonBody == nil {
+      if error, ok := v["error"]; ok {
+        status, _ := v["status"]
+        return body, fmt.Errorf("Error [%s] Status [%v]", error, status)
+      }
+    }
+    return body, jsonBody
+  }
+  return
+}
+
+func buildRequest(method, url, data string) (req *http.Request, err error) {
+  req, err = http.NewRequest(method, url, strings.NewReader(data))
   if err != nil {
     return
   }
 
   req.Header.Set("Content-Type", "application/json")
   req.Header.Add("Accept", "application/json")
-  setBody(req, strings.NewReader(data))
 
-  httpStatusCode, body, err = do(req, &response)
-  if err != nil {
-    return
-  }
-
-  if httpStatusCode > 304 {
-
-    jsonErr := json.Unmarshal(body, &response)
-    if jsonErr == nil {
-      if error, ok := response["error"]; ok {
-        status, _ := response["status"]
-        return body, fmt.Errorf("Error [%s] Status [%v]", error, status)
-      }
-    }
-    return body, jsonErr
-  }
   return
-}
-
-func setBody(req *http.Request, body io.Reader) {
-  rc, ok := body.(io.ReadCloser)
-  if !ok && body != nil {
-    rc = ioutil.NopCloser(body)
-  }
-  req.Body = rc
-  if body != nil {
-    switch v := body.(type) {
-    case *strings.Reader:
-      req.ContentLength = int64(v.Len())
-    }
-  }
-}
-
-func do(req *http.Request, v interface{}) (int, []byte, error) {
-  response, bodyBytes, err := processResponse(req, v)
-  if err != nil {
-    return -1, nil, err
-  }
-  return response.StatusCode, bodyBytes, err
-}
-
-func processResponse(req *http.Request, v interface{}) (*http.Response, []byte, error) {
-  res, err := http.DefaultClient.Do(req)
-  if err != nil {
-    return nil, nil, err
-  }
-
-  defer res.Body.Close()
-  bodyBytes, err := ioutil.ReadAll(res.Body)
-
-  if err != nil {
-    return nil, nil, err
-  }
-
-  if res.StatusCode > 304 && v != nil {
-    jsonErr := json.Unmarshal(bodyBytes, v)
-    if jsonErr != nil {
-      return nil, nil, jsonErr
-    }
-  }
-  return res, bodyBytes, err
 }
